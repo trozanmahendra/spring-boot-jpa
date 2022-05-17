93f1b8f03b76486b4992afbb6fc6e588df77e540
MahendraDXC <105200048+MahendraDXC@users.noreply.github.com>
2022-05-17 19:58:31 +0530

    Updated with testcases, new repository methods and reference links in comments

--- src/main/java/com/mgWorks/springBoot/JPA/Repository/StudentRepository.java
+++ src/main/java/com/mgWorks/springBoot/JPA/Repository/StudentRepository.java
@@ -1,10 +1,19 @@
 package com.mgWorks.springBoot.JPA.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.mgWorks.springBoot.JPA.entity.Student;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+	public List<Student> findByFirstName(String firstName);
+	public List<Student> findByFirstNameContaining(String name);
+	public List<Student> findByLastNameNotNull();
+	public List<Student> findByGaurdianName(String gaurdianName);
+	
+//	refernce documents to create different methods here below
+//	https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repositories.query-methods.query-creation
 
 }
--- src/test/java/com/mgWorks/springBoot/JPA/Repository/test/StudentRepositoryTest.java
+++ src/test/java/com/mgWorks/springBoot/JPA/Repository/test/StudentRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.mgWorks.springBoot.JPA.Repository.StudentRepository;
+import com.mgWorks.springBoot.JPA.entity.Gaurdian;
 import com.mgWorks.springBoot.JPA.entity.Student;
 
 @SpringBootTest
@@ -14,21 +15,59 @@ import com.mgWorks.springBoot.JPA.entity.Student;
 class StudentRepositoryTest {
 	@Autowired
 	private StudentRepository studentRepository;
+	
 	@Test
 	public void SaveStudent() {
 		Student student = Student.builder()
-				.emailId("mg@mail.com")
-				.firstName("Mahendra")
-				.gaurdianEmail(null)
-				.gaurdianMobile("8899667755")
-				.gaurdianName("lkn")
-				.Lastname("goud")
+				.emailId("mg123@mail.com")
+				.firstName("mahi")
+//				.gaurdianEmail(null)
+//				.gaurdianMobile("8899667755")
+//				.gaurdianName("lkn")
+				.lastName("goud")
 				.build();
 		studentRepository.save(student);
 		}
+	@Test
+	public void saveStudentWithgaurdian() {
+		Gaurdian gaurdian = Gaurdian.builder()
+				.email("jaya@mail.com")
+				.mobile("5566445522")
+				.name("jaya")
+				.build();
+		
+		Student student = Student.builder()
+				.emailId("mg123@mail.com")
+				.firstName("mahindra")
+				.gaurdian(gaurdian)
+				.lastName("goud")
+				.build();
+		studentRepository.save(student);
+		}
+	
 	@Test
 	public void printAll() {
 		List<Student> studentList = studentRepository.findAll();
 		System.out.println("studentList :- "+studentList);
 	}
+	
+	@Test
+	public void printStudentByFirstName() {
+		List<Student> students = studentRepository.findByFirstName("mahindra");
+		List<Student> studentsContaining = studentRepository.findByFirstNameContaining("hind");
+		System.out.println("StudentsListedByTheirFirstnames :- "+students);
+		System.out.println("StudentsListedByTheirFirstnamesContaining :- "+studentsContaining);
+	}
+	@Test
+	public void printStudentByGaurdianname() {
+		List<Student> studentsGaurdian = studentRepository.findByGaurdianName("jaya");
+		System.out.println("StudentsListedByTheirGaurdianName :- "+studentsGaurdian);
+
+	}
+	
+	
+	
+	
+	
+	
 }
